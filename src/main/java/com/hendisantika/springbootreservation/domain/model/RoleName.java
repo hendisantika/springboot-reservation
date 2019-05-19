33d5cadc2fb77059ea13f732f9cba1157c6c0981
hendisantika <hendisantika@yahoo.co.id>
2019-05-20 04:29:35 +0700

    #12 - Add RoleName Domain Model

---
+++ src/main/java/com/hendisantika/springbootreservation/domain/model/RoleName.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootreservation.domain.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-20
+ * Time: 04:29
+ */
+public enum RoleName {
+    ADMIN, USER
+
+}

57d6f22370df600a950c3aca80ddf4cc7eae4d9c
hendisantika <hendisantika@yahoo.co.id>
2019-05-20 04:29:48 +0700

    #13 - Add User Domain Model

---
+++ src/main/java/com/hendisantika/springbootreservation/domain/model/User.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootreservation.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-20
+ * Time: 04:28
+ */
+@Entity
+@Table(name = "usr")
+@Data
+public class User {
+
+    @Id
+    private String userId;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+}

8210b072d73961987e39c066aab70182b398f6fe
hendisantika <hendisantika@yahoo.co.id>
2019-05-20 04:13:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootreservation/SpringbootReservationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReservationApplication.class, args);
+    }
+
+}

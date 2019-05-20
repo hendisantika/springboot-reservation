997ffafeddb4d1edc0fe2fabb0c003036cc55ac5
hendisantika <hendisantika@yahoo.co.id>
2019-05-21 05:35:12 +0700

    #26 - Add ReservationUserDetails

---
+++ src/main/java/com/hendisantika/springbootreservation/service/ReservationUserDetails.java
@@ -0,0 +1,68 @@
+package com.hendisantika.springbootreservation.service;
+
+import com.hendisantika.springbootreservation.domain.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-21
+ * Time: 05:34
+ */
+public class ReservationUserDetails implements UserDetails {
+
+    private final User user;
+
+    public ReservationUserDetails(User user) {
+
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("ROLE_" + this.user.getRoleName().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}

f708e4911a2e02e07601893266ee4808cd8dd839
hendisantika <hendisantika@yahoo.co.id>
2019-05-22 05:26:21 +0700

    #36 - Add ThirtyMinutesUnitValidator

---
+++ src/main/java/com/hendisantika/springbootreservation/annotation/ThirtyMinutesUnitValidator.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootreservation.annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-22
+ * Time: 05:24
+ */
+public class ThirtyMinutesUnitValidator implements ConstraintValidator<ThirtyMinutesUnit, LocalTime> {
+
+    @Override
+    public void initialize(ThirtyMinutesUnit constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(LocalTime value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        return value.getMinute() % 30 == 0;
+    }
+
+
+}

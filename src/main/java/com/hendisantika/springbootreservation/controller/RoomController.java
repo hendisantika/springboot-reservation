e49e53835879242e59fdbe875ee76f57ef3fb486
hendisantika <hendisantika@yahoo.co.id>
2019-05-22 05:21:12 +0700

    #34 - Add listRooms method in RoomController

--- src/main/java/com/hendisantika/springbootreservation/controller/RoomController.java
+++ src/main/java/com/hendisantika/springbootreservation/controller/RoomController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springbootreservation.controller;
 
+import com.hendisantika.springbootreservation.domain.model.ReservableRoom;
 import com.hendisantika.springbootreservation.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,25 @@ public class RoomController {
 
     @Autowired
     RoomService roomService;
+
+    /**
+     * Return of today's reservation list(/rooms)
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    String listRooms(Model model) {
+        LocalDate today = LocalDate.now();
+        //Return today's reservation list
+        List<ReservableRoom> rooms = roomService.findReservableRooms(today);
+
+        model.addAttribute("date", today);
+        model.addAttribute("rooms", rooms);
+
+        return "room/listRooms";
+
+    }
+
 
 }
